88a45849681e5f4cc76fc67ba22ec554a15ca785
FurugenYu <furugen098@gmail.com>
2017-06-19 16:02:35 +0900

    add

---
+++ MKSample/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MKSample
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
